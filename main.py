6a1c242218b618a363674a20571e8e4060ecc507
milagro2 <124157019+milagro2@users.noreply.github.com>
2023-12-21 15:30:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+import os
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello, World!"}
+    return {"message": "Hello World"}
 
 @app.get("/hello")
 def read_hello():
     return {"message": "Hello!"}
+
+if __name__ == "__main__":
+    import uvicorn
+
+    port = int(os.getenv("PORT", default=8000))
+
+    uvicorn.run(app, host="0.0.0.0", port=port)
