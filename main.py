128eb1c24c302569d796d1dd7e253af4ce085845
milagro2 <124157019+milagro2@users.noreply.github.com>
2024-01-08 14:50:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,23 @@
 import os
-from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import JSONResponse
-from PIL import Image
+from fastapi import FastAPI
 
 app = FastAPI()
+
 
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
-    
+
+
 @app.get("/hello")
 def read_hello():
     return {"message": "Hello!"}
 
-@app.post("/get_dimensions")
-async def get_dimensions(file: UploadFile = File(...)):
-    try:
-        contents = await file.read()
-        image = Image.open(io.BytesIO(contents))
-        width, height = image.size
-        return JSONResponse(content={"width": width, "height": height}, status_code=200)
-    except Exception as e:
-        return JSONResponse(content={"error": str(e)}, status_code=500)
+
+@app.get("/get_dimensions")
+def read_hello():
+    return {"message": "Bye Bye!"}
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=8000))
