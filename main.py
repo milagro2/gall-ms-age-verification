c0cc9df1532964535dcc134860f2dbad04fa07a4
milagro2 <124157019+milagro2@users.noreply.github.com>
2024-01-08 15:50:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,89 +1,20 @@
-from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import HTMLResponse
-from starlette.staticfiles import StaticFiles
-from pathlib import Path
-import shutil
-import uuid
-from typing import List
+import os
+from fastapi import FastAPI
 
 app = FastAPI()
 
-# Create an 'uploads' directory to store the uploaded files
-upload_dir = Path("uploads")
-upload_dir.mkdir(exist_ok=True)
-
-# Mount the 'uploads' directory as a static file directory
-app.mount("/uploads", StaticFiles(directory=upload_dir), name="uploads")
-
-# Dictionary to store uploaded files information
-uploaded_files = {}
-
-
 @app.get("/")
 def read_root():
-    return {"message": "Hello, FastAPI!"}
+    return {"message": "Hello World"}
 
+@app.get("/hello")
+def read_hello():
+    return {"message": "Hello!"}
 
-@app.post("/uploadfile/", response_class=HTMLResponse)
-async def create_upload_file(files: List[UploadFile] = File(...)):
-    try:
-        for file in files:
-            unique_filename = str(uuid.uuid4()) + "_" + file.filename
-            file_path = upload_dir / unique_filename
-            with file_path.open("wb") as buffer:
-                shutil.copyfileobj(file.file, buffer)
-            uploaded_files[unique_filename] = file.filename
-
-        return HTMLResponse(content=f"""
-        <html>
-            <head>
-                <title>Upload Successful</title>
-            </head>
-            <body>
-                <h1>Upload Successful</h1>
-                <p>Uploaded files:</p>
-                <ul>
-                    {"".join([f"<li>{filename}</li>" for filename in uploaded_files.values()])}
-                </ul>
-                <p><a href="/showfile">View Uploaded Files</a></p>
-            </body>
-        </html>
-        """)
-    except Exception as e:
-        return HTMLResponse(content=f"""
-        <html>
-            <head>
-                <title>Error</title>
-            </head>
-            <body>
-                <h1>Error Uploading Files: {e}</h1>
-                <p><a href="/">Go back to home</a></p>
-            </body>
-        </html>
-        """)
-
-
-@app.get("/showfile", response_class=HTMLResponse, description="View all uploaded images.")
-async def show_uploaded_files():
-    images = []
-    for display_filename in uploaded_files.values():
-        images.append(f"<img src='/uploads/{display_filename}' style='max-width: 300px; max-height: 300px;'>")
-
-    return HTMLResponse(content=f"""
-    <html>
-        <head>
-            <title>Uploaded Images</title>
-        </head>
-        <body>
-            <h1>Uploaded Images</h1>
-            {''.join(images)}
-            <p><a href="/">Go back to home</a></p>
-        </body>
-    </html>
-    """)
+@app.get("/get_dimensions")
+def read_hello():
+    return {"message": "Bye Bye!"}
 
 
 if __name__ == '__main__':
-    import uvicorn
-
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    app.run(debug=True, port=os.getenv("PORT", default=8000))
