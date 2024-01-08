19ad590f1f70f6486d08d3adcce6a536b16c4001
milagro2 <124157019+milagro2@users.noreply.github.com>
2024-01-08 14:54:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
 
 app = FastAPI()
 
@@ -17,6 +17,19 @@ def read_hello():
 @app.get("/get_dimensions")
 def read_hello():
     return {"message": "Bye Bye!"}
+
+@app.post("/upload")
+def upload(file: UploadFile = File(...)):
+    try:
+        contents = file.file.read()
+        with open(file.filename, 'wb') as f:
+            f.write(contents)
+    except Exception:
+        return {"message": "There was an error uploading the file"}
+    finally:
+        file.file.close()
+
+    return {"message": f"Successfully uploaded {file.filename}"}
 
 
 if __name__ == '__main__':
