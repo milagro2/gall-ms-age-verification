815c9023e3a259e017aa0ad5bfedbb612ea90e71
milagro2 <124157019+milagro2@users.noreply.github.com>
2024-01-08 13:27:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,12 @@
 import os
-from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
-from pathlib import Path
-from PIL import Image
+from fastapi import FastAPI
 
 app = FastAPI()
-
-upload_dir = Path("uploads")
-upload_dir.mkdir(exist_ok=True)
-
-app.mount("/uploads", StaticFiles(directory=upload_dir), name="uploads")
 
 
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
-    
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile = File(...)):
-    file_path = upload_dir / file.filename
-    with file_path.open("wb") as buffer:
-        buffer.write(file.file.read())
-
-    dimensions = get_image_dimensions(file_path)
-
-    return {"filename": file.filename, "dimensions": dimensions}
-
-
-def get_image_dimensions(image_path: Path):
-    try:
-        with Image.open(image_path) as img:
-            width, height = img.size
-            return {"width": width, "height": height}
-    except Exception as e:
-        return {"error": f"Unable to get dimensions: {e}"}
 
 @app.get("/get_dimensions")
 def read_hello():
