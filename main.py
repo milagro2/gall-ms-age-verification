8ae941ec5baff91a03384b5d4a0af8ef73db8751
milagro2 <124157019+milagro2@users.noreply.github.com>
2024-01-08 15:24:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,20 @@ def read_root():
 def read_hello():
     return {"message": "Hello!"}
 
+@app.post("/uploadfile/", response_class=HTMLResponse)
+async def create_upload_file(files: List[UploadFile] = File(...)):
+    try:
+        for file in files:
+            unique_filename = str(uuid.uuid4()) + "_" + file.filename
+            file_path = upload_dir / unique_filename
+            with file_path.open("wb") as buffer:
+                shutil.copyfileobj(file.file, buffer)
+            uploaded_files[unique_filename] = file.filename
 
+        return RedirectResponse(url="/showfile")
+    except Exception as e:
+        return {"error": f"An error occurred: {e}"}
+        
 @app.get("/get_dimensions")
 def read_hello():
     return {"message": "Bye Bye!"}
