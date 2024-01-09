21f6de4ebd5b7c1a7ce3a5fc24b3848b307923fb
milagro2 <124157019+milagro2@users.noreply.github.com>
2024-01-09 14:29:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,29 +3,26 @@ from fastapi import FastAPI, File, UploadFile, HTTPException
 
 app = FastAPI()
 
-# Ensure the uploads directory exists
 os.makedirs("uploads", exist_ok=True)
 
 def get_image_dimensions(file_path: str):
     try:
-        # Open the image using Pillow (PIL)
         from PIL import Image
         with Image.open(file_path) as img:
-            # Get image dimensions
             width, height = img.size
             return width, height
+
     except Exception as e:
         raise HTTPException(status_code=400, detail=f"Error analyzing image dimensions: {str(e)}")
 
-@app.post("/uploadfile/")
+@app.post("/get_dimensions/")
 async def create_upload_file(file: UploadFile = File(...)):
     try:
-        # Save the uploaded file
+
         file_path = os.path.join("uploads", file.filename)
         with open(file_path, "wb") as buffer:
             buffer.write(file.file.read())
 
-        # Get image dimensions
         width, height = get_image_dimensions(file_path)
 
         return {"filename": file.filename, "width": width, "height": height}
